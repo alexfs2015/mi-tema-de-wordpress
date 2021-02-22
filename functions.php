2915f56eac4a754a00f08f7db66a02f43d36edf3
alexfs2015 <44406987+alexfs2015@users.noreply.github.com>
2021-02-22 14:00:26 +0100

    Add files via upload

--- functions.php
+++ functions.php
@@ -32,6 +32,20 @@
             )
         );
 
+        // título
+        $wp_customize->add_setting( 'titulo', array(
+            'capability' => 'edit_theme_options',
+            'default' => 'Título',
+            'sanitize_callback' => 'sanitize_text_field',
+          ) );
+          
+        $wp_customize->add_control( 'titulo', array(
+            'type' => 'text',
+            'section' => 'mytheme_header_options', // Add a default or your own section
+            'label' => __( 'Título de la web' ),
+            'description' => __( 'Aquí se pone el título de la web.' ),
+          ) );
+
         // Background color
         $wp_customize->add_setting( 'header_bg_color',
 	        array(
@@ -72,6 +86,64 @@
                 )
             )
         );
+
+        $font_choices = array(
+			'Source Sans Pro:400,700,400italic,700italic' => 'Source Sans Pro',
+			'Open Sans:400italic,700italic,400,700' => 'Open Sans',
+			'Oswald:400,700' => 'Oswald',
+			'Playfair Display:400,700,400italic' => 'Playfair Display',
+			'Montserrat:400,700' => 'Montserrat',
+			'Raleway:400,700' => 'Raleway',
+			'Droid Sans:400,700' => 'Droid Sans',
+			'Lato:400,700,400italic,700italic' => 'Lato',
+			'Arvo:400,700,400italic,700italic' => 'Arvo',
+			'Lora:400,700,400italic,700italic' => 'Lora',
+			'Merriweather:400,300italic,300,400italic,700,700italic' => 'Merriweather',
+			'Oxygen:400,300,700' => 'Oxygen',
+			'PT Serif:400,700' => 'PT Serif',
+			'PT Sans:400,700,400italic,700italic' => 'PT Sans',
+			'PT Sans Narrow:400,700' => 'PT Sans Narrow',
+			'Cabin:400,700,400italic' => 'Cabin',
+			'Fjalla One:400' => 'Fjalla One',
+			'Francois One:400' => 'Francois One',
+			'Josefin Sans:400,300,600,700' => 'Josefin Sans',
+			'Libre Baskerville:400,400italic,700' => 'Libre Baskerville',
+			'Arimo:400,700,400italic,700italic' => 'Arimo',
+			'Ubuntu:400,700,400italic,700italic' => 'Ubuntu',
+			'Bitter:400,700,400italic' => 'Bitter',
+			'Droid Serif:400,700,400italic,700italic' => 'Droid Serif',
+			'Roboto:400,400italic,700,700italic' => 'Roboto',
+			'Open Sans Condensed:700,300italic,300' => 'Open Sans Condensed',
+			'Roboto Condensed:400italic,700italic,400,700' => 'Roboto Condensed',
+			'Roboto Slab:400,700' => 'Roboto Slab',
+			'Yanone Kaffeesatz:400,700' => 'Yanone Kaffeesatz',
+			'Rokkitt:400' => 'Rokkitt',
+		);
+
+        $wp_customize->add_setting( 'selector_fuente',
+            array(
+                'default'    => 'default',
+                'type'       => 'theme_mod',
+                'capability' => 'edit_theme_options',
+            ) 
+            );
+
+            $wp_customize->add_control( new WP_Customize_Control(
+                $wp_customize,
+                'mytheme',
+                array(
+                   'label'      => __( 'Select Theme Name', 'mytheme' ),
+                   'description' => __( 'Using this option you can change the theme colors' ),
+                   'settings'   => 'selector_fuente',
+                   'priority'   => 10,
+                   'section'    => 'mytheme_header_options',
+                   'type'    => 'select',
+                   'choices' => $font_choices
+               )
+               ) );
+
+
+        
 
     }
     
--- header.php
+++ header.php
@@ -1,10 +1,12 @@
 <html>
     <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        
     </head>
     <style>
         .navbar-custom{
             background-color: <?php echo get_theme_mod('header_bg_color')?>;
+            font-family: "<?php echo get_theme_mod('selector_fuente') ?>";
         }
     </style>
     <body>
@@ -13,8 +15,15 @@
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">
                 <?php if (get_theme_mod('brand') == 'logo'){
-                    ?><img src="<?php get_theme_mod('image')?>" width="40" height="40">
-                <?php } ?>
+                    ?><img src="<?php echo get_theme_mod('image')?>" width="40" height="40"></a>
+                <?php } 
+                elseif (get_theme_mod('brand') == 'nombre'){
+                    echo get_theme_mod('titulo')?></a><?php
+                }
+                elseif (get_theme_mod('brand') == 'nada'){
+                    ?></a><?php
+                }
+                ?>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -41,17 +50,11 @@
                         <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
                     </li>
                 </ul>
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
                 </div>
             </div>
         </nav>
         <!-------------------------------------------------->
-        <?php echo get_theme_mod('header_bg_color')?>
-        <?php echo get_theme_mod('brand')?>
-        <?php echo get_theme_mod('image')?>
+        <?php esc_html_e(get_theme_mod('selector_tipografia')) ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
     </body>
 </html>
